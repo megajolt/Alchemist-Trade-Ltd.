d95623b69eada1631e342df9f4f554df37161b40
Quinn Hoffman <31490850+megajolt@users.noreply.github.com>
2024-07-19 17:51:37 -0700

    Basic stuff for changing money amount

--- Scenes/HUD.gd
+++ Scenes/HUD.gd
@@ -1,6 +1,8 @@
 extends Control
 
 var money = 5000
+var moneyChange=false
+var changeAmount=0
 var day = 1
 var fulfilledContracts = 0
 var contractsRequired = 4
@@ -13,6 +15,8 @@ var minute = Date.minute
 var second = Date.second
 var pause = false
 var fast = false
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Money.text = "$: "+str(money)
@@ -22,12 +26,15 @@ func _ready():
 	$"Fulfilled Contracts".text="Contracts: "+str(fulfilledContracts)+"/"+str(contractsRequired)
 	$Date.text = str(calMonth)+"/"+str(calDay)+"/"+str(calYear)
 	$Time.text = str(hour)+":"+str(minute)+":"+str(second)
-	pass # Replace with function body.
+
+
 func _contract_check():
 	if(day%7==0&&fulfilledContracts!=contractsRequired||money<-10000):
 		$GameOver.visible=true
 	else:
-		$GameOver.visible=false
+		$GameOver.visible=false	
+
+
 func _update_time():
 	if($FFWD.button_pressed):
 		second=second+4
@@ -42,11 +49,21 @@ func _update_time():
 		minute=0
 		hour=hour+1
 	$Time.text = str(hour)+":"+str(minute)+":"+str(second)
-	pass
+
+
+func _input(event):
+	if(event.is_action_pressed("Add Money")):
+		changeAmount+=500
+		moneyChange=true
+	if(event.is_action_pressed("Subtract Money")):
+		changeAmount-=500
+		moneyChange=true
+
+
 func _update_date():
-	if($"Pass Day".button_pressed||hour==24):
-		$"Pass Day".button_pressed=false
-		$"Pass Day".disabled = true
+	if($" Day".button_pressed||hour==24):
+		$" Day".button_pressed=false
+		$" Day".disabled = true
 		day=day+1
 		calDay=calDay+1
 		hour=8
@@ -61,29 +78,47 @@ func _update_date():
 	$Day.text = "Day: "+str(day)
 	$Date.text = str(calMonth)+"/"+str(calDay)+"/"+str(calYear)
 	$Time.text = str(hour)+":"+str(minute)+":"+str(second)
-	$"Pass Day".disabled=false
-	pass
+	$" Day".disabled=false
+
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	_contract_check()
 	_update_date()
 	_update_time()
-	pass
+	if(moneyChange==true&&changeAmount!=0):
+		if(changeAmount<0):
+			$Subtract.text="- "+str(abs(changeAmount))
+			$Subtract.visible=true
+			money-=1
+			changeAmount+=1
+			$Money.text="$: "+str(money)
+		if(changeAmount>0):
+			$Add.text="+ "+str(changeAmount)
+			$Add.visible=true
+			money+=1
+			changeAmount-=1
+			$Money.text="$: "+str(money)
+	if(changeAmount==0):
+		$Add.visible=false
+		$Subtract.visible=false
+		moneyChange=false
+	if(money<0):
+		$Money.add_theme_color_override("font_color","#cf0000")
+	elif(money>=0):
+		$Money.add_theme_color_override("font_color","#00cf00")
 
 
 func _on_pause_pressed():
 	$Play.button_pressed=false
 	$FFWD.button_pressed=false
-	pass # Replace with function body.
 
 
 func _on_play_pressed():
 	$FFWD.button_pressed=false
 	$Pause.button_pressed=false
-	pass # Replace with function body.
+
+
 func _on_ffwd_pressed():
 	$Pause.button_pressed=false
 	$Play.button_pressed=false
-	pass # Replace with function body.
-
-
